3eb5ece11ab05aa7ba3aa80d0fd6a987d19c14e6
ParsaHmi <158472908+ParsaHmi@users.noreply.github.com>
2025-06-28 15:14:37 +0330

    make new root page named authentication

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('authentication');
+});
+
+Route::get('/root', function () {
     return view('welcome');
 });
